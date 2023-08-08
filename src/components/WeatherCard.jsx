59b5b9ce9716f1b5986cfb518d394be59d0549a3
Vincenzo Cristiano <viulenzgg@gmail.com>
2023-08-08 18:47:42 +0200

    weather card now uses fetched data

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,7 +1,24 @@
+import PropTypes from "prop-types";
 import PlaceHolder from "../assets/icons/mapIcon.png";
 import MapPin from "../assets/icons/mapPin.png";
+import { useEffect } from "react";
 
-export default function WeatherCard() {
+WeatherCard.propTypes = {
+  weatherData: PropTypes.object,
+};
+
+export default function WeatherCard({ weatherData }) {
+
+  const sunset = new Date(weatherData.sys.sunset * 1000);
+  const sunsetHours = sunset.getHours().toString().padStart(2, "0");
+  const sunsetMinutes = sunset.getMinutes().toString().padStart(2, "0");
+
+  const sunrise = new Date(weatherData.sys.sunrise * 1000);
+  const riseHours = sunrise.getHours().toString().padStart(2, "0");
+  const riseMinutes = sunrise.getMinutes().toString().padStart(2, "0");
+
+  useEffect(() => {}, [weatherData]);
+
   return (
     <section className='p-4 border-2 rounded-lg border-slider'>
       <div className='flex items-center'>
@@ -10,11 +27,11 @@ export default function WeatherCard() {
         </div>
         <div className='ml-2'>
           <p className='text-light-text uppercase mb-1'>Weather</p>
-          <p className='font-semibold text-header-text'>placeholder</p>
+          <p className='font-semibold lowercase text-header-text'>{weatherData.weather[0].main}</p>
         </div>
         <div className='ml-4'>
           <p className='text-light-text uppercase mb-1'>Description</p>
-          <p className='font-semibold text-header-text'>placeholder</p>
+          <p className='font-semibold text-header-text'>{weatherData.weather[0].description}</p>
         </div>
       </div>
 
@@ -22,18 +39,22 @@ export default function WeatherCard() {
 
       <div className='flex items-center gap-8 mb-4'>
         <div>
-          <p className='text-light-text uppercase mb-1'>Sunset</p>
-          <p className='font-semibold text-header-text'>placeholder</p>
+          <p className='text-light-text uppercase mb-1'>Sunrise</p>
+          <p className='font-semibold text-header-text'>
+            {riseHours}:{riseMinutes}
+          </p>
         </div>
         <div>
-          <p className='text-light-text uppercase mb-1'>Sunrise</p>
-          <p className='font-semibold text-header-text'>Placeholder</p>
+          <p className='text-light-text uppercase mb-1'>Sunset</p>
+          <p className='font-semibold text-header-text'>
+            {sunsetHours}:{sunsetMinutes}
+          </p>
         </div>
         <div>
           <p className='text-light-text uppercase mb-1'>Location</p>
           <p className='font-semibold text-header-text flex items-center gap-1'>
             <img src={MapPin} alt='' />
-            Placeholder
+            {weatherData.name}
           </p>
         </div>
       </div>
@@ -41,11 +62,11 @@ export default function WeatherCard() {
       <div className='flex items-center gap-8 mb-2'>
         <div>
           <p className='text-light-text uppercase mb-1'>Temperature</p>
-          <p className='font-semibold text-header-text'>placeholder</p>
+          <p className='font-semibold text-header-text'>{weatherData.main.temp} °C</p>
         </div>
         <div>
           <p className='text-light-text uppercase mb-1'>Feels like</p>
-          <p className='font-semibold text-header-text'>placeholder</p>
+          <p className='font-semibold text-header-text'>{weatherData.main.feels_like} °C</p>
         </div>
       </div>
 
--- src/pages/WeatherCity.jsx
+++ src/pages/WeatherCity.jsx
@@ -1,21 +1,44 @@
 import locations from "../locations.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import City from "../components/City";
 import WeatherCard from "../components/WeatherCard";
+import axios from "axios";
+import LoadingSpinner from "../components/LoadingSpinner";
+
+const API_KEY = "37f2111fdeb0f75bcb28fbd30c3c518c";
 
 export default function WeatherCity() {
   const [city, setCity] = useState(locations[0]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleCity = e => {
     const userSelection = e.target.value;
     setCity(locations.find(city => city.city === userSelection));
   };
-  console.log(city);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.location.coordinates[0]}&lon=${city.location.coordinates[1]}&appid=${API_KEY}&units=metric`);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [city]);
+
   return (
     <div className='container mx-auto px-2'>
       <h1 className='text-3xl font-bold text-center text-header-text py-2 '>Weather City</h1>
-      <City locations={locations} handleCity={handleCity}  />
-      <WeatherCard />
+      <City locations={locations} handleCity={handleCity} />
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && data && <WeatherCard weatherData={data} />}
+      {error && <h2 className='text-3xl font-bold text-center text-error-text py-2 '>{error}</h2>}
     </div>
   );
 }
