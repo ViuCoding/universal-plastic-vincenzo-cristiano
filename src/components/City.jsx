21256e325fb6eac871909f8d056ab7d6ffcffe23
Vincenzo Cristiano <viulenzgg@gmail.com>
2023-08-08 16:27:59 +0200

    added city select component

---
+++ src/components/City.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+City.propTypes = {
+  locations: PropTypes.array,
+  handleCity: PropTypes.func,
+};
+
+export default function City({ locations, handleCity }) {
+  return (
+    <section>
+      <h2 className='text-2xl font-bold text-header-text py-2 '>City</h2>
+      <select name='locations' id='locations' className='bg-input-bg text-input-text font-medium p-3 focus:outline-none w-full rounded-lg' onChange={handleCity}>
+        {locations.map(city => {
+          return (
+            <option key={city.id} value={city.city} className='outline-none'>
+              {city.city}
+            </option>
+          );
+        })}
+      </select>
+    </section>
+  );
+}
--- src/pages/WeatherCity.jsx
+++ src/pages/WeatherCity.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import City from "../components/City";
+import locations from "../locations.json";
+
 export default function WeatherCity() {
-  return <h1>Weather City</h1>;
+  const [city, setCity] = useState(locations[0]);
+
+  const handleCity = e => {
+    const userSelection = e.target.value;
+    setCity(locations.find(city => city.city === userSelection));
+  };
+  console.log(city);
+  return (
+    <div className='container mx-auto px-2'>
+      <h1 className='text-3xl font-bold text-center text-header-text py-2 '>Weather City</h1>
+      <City locations={locations} handleCity={handleCity} />
+    </div>
+  );
 }
